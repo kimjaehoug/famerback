f596fea571ded973c10157c4e0f7e201b8d0640f
louisjeon <wjss5115@gmail.com>
2025-02-14 04:54:09 +0900

    v1

--- src/controllers/consulting.controller.js
+++ src/controllers/consulting.controller.js
@@ -1,7 +1,7 @@
 const { default: axios } = require("axios");
 
 exports.consult = async (req, res) => {
-  console.log(req.body);
+  console.log("AAAAAA");
   const {
     companyName,
     industry,
@@ -13,30 +13,39 @@ exports.consult = async (req, res) => {
     landSize,
     rent,
   } = req.body;
+  console.log("DDDDD");
+
+  const b = {
+    business_data: [
+      {
+        특구종류: "규제자유특구",
+        기업명: companyName,
+        산업군: industry,
+        "사업 설명": "블록체인을 이용하는 기업이다.",
+        "투자 규모 (억원)": investmentScale,
+        인프라: infras,
+        법인세: corpTax,
+        재산세: propertyTax,
+        부지크기: landSize,
+        임대료: rent,
+      },
+    ],
+  };
+
+  console.log(explanation);
 
   await axios
-    .post(`${process.env.PYTHON_API}/recommend`, {
-      business_data: [
-        {
-          특구종류: "규제자유특구",
-          기업명: companyName,
-          산업군: industry,
-          "사업 설명": explanation,
-          "투자 규모 (억원)": investmentScale,
-          인프라: infras,
-          법인세: corpTax,
-          제산세: propertyTax,
-          부지크기: landSize,
-          임대료: rent,
-        },
-      ],
-    })
+    .post(`${process.env.PYTHON_API}/recommend/`, b)
     .then((data) => {
-      console.log(data);
+      console.log("BBBBB");
       res.status(200).json({ data: data.data });
     })
     .catch((err) => {
       console.log(err);
-      res.status(400).json({ err });
+      if (err.status === 500) {
+        res.status(200).json({ data: null });
+      } else {
+        res.status(400).json({ err });
+      }
     });
 };
