48dccce21610f0c1e2401591ec1f07fa53a9041d
louisjeon <wjss5115@gmail.com>
2025-02-14 00:48:41 +0900

    change jaehong server connection

--- src/controllers/consulting.controller.js
+++ src/controllers/consulting.controller.js
@@ -4,24 +4,30 @@ exports.consult = async (req, res) => {
   console.log(req.body);
   const {
     companyName,
-    districtType,
     industry,
     infras,
     investmentScale,
     explanation,
-    author,
+    corpTax,
+    propertyTax,
+    landSize,
+    rent,
   } = req.body;
 
   await axios
     .post(`${process.env.PYTHON_API}/recommend`, {
       business_data: [
         {
-          특구종류: districtType,
+          특구종류: "규제자유특구",
           기업명: companyName,
           산업군: industry,
           "사업 설명": explanation,
           "투자 규모 (억원)": investmentScale,
           인프라: infras,
+          법인세: corpTax,
+          제산세: propertyTax,
+          부지크기: landSize,
+          임대료: rent,
         },
       ],
     })
